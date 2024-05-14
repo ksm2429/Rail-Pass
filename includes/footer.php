961cabaad679ba5c99286b883b2545750a4d7e73
ksm2429 <150602167+ksm2429@users.noreply.github.com>
2024-05-14 22:33:30 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+	<div class="copy-right"> 
+		<div class="container">
+			<p>© Railway Pass Management System</p>
+		</div> 
+	</div> 
+	<!-- //footer -->   
+	
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<div class="top-nav w3-agiletop">
+                <div class="container">
+                    <div class="navbar-header w3llogo">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>  
+                        <h1><a href="index.php">Railway Pass Management System</a></h1> 
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <div class="w3menu navbar-right">
+                            <ul class="nav navbar">
+                                <li><a href="index.php" class="active">Home</a></li>
+                                <li><a href="about.php">About</a></li> 
+                                                             
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="download-pass.php">View Pass</a></li>
+                                <li><a href="admin/index.php">Admin</a></li>
+                            </ul>
+                        </div> 
+                        <div class="clearfix"> </div>  
+                    </div>
+                </div>  
+            </div>
